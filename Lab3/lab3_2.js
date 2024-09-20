93d44532a5f630087b9bc9dee307608ebc8e275b
Derrick1013 <j24041772@student.newinti.edu.my>
2024-09-20 21:55:03 +0800

    Update Lab 3.1 and Lab 3.2 files with demo 2 and added additional styling and book.css

--- Lab3/lab3_2.js
+++ Lab3/lab3_2.js
@@ -1,4 +1,4 @@
-var dataset = [];
+/* var dataset = [];
 var numDataPoints = 50;
 var xRange = Math.random() * 1000;
 var yRange = Math.random() * 1000;
@@ -6,14 +6,20 @@ for (var i = 0; i < numDataPoints; i++) {
    var newNumber1 = Math.floor(Math.random() * xRange);
    var newNumber2 = Math.floor(Math.random() * yRange);
    dataset.push([newNumber1, newNumber2]);
-}
+} */
+
+
+var dataset = [[2,8], [3,5], [5,17],[6,6], [6,12], [7,20], [8,22], [10,11], [5,12], [6,16]]
+
 
 console.log(dataset);
 
+
 var w = 500;
 var h = 300;
-var padding = 30;  // Increased padding to accommodate axis labels
+var padding = 50;  // Increased padding to accommodate axis labels
 
+// Scales for x and y axes
 var xScale = d3.scaleLinear()
     .domain([0, d3.max(dataset, function(d) { return d[0]; })])
     .range([padding, w - padding]);
@@ -25,8 +31,10 @@ var yScale = d3.scaleLinear()
 var svg = d3.select("#chart")
             .append("svg")
             .attr("width", w)
-            .attr("height", h);
+            .attr("height", h)
+            .attr("transform", "translate(50, 0)");
 
+// Create circles for scatter plot
 svg.selectAll("circle")
     .data(dataset)
     .enter()
@@ -40,12 +48,15 @@ svg.selectAll("circle")
     .attr("r", 5)
     .style("fill", "red");
 
+    // Create labels for scatter plot
 var xAxis = d3.axisBottom(xScale)
 .ticks(5);
 
 var yAxis = d3.axisLeft(yScale)
 .ticks(5);
 
+
+// Create axes
 svg.append("g")
     .call(xAxis)
     .attr("class", "axis")
@@ -55,3 +66,21 @@ svg.append("g")
     .call(yAxis)
     .attr("class", "axis")
     .attr("transform", "translate(" + padding + ",0)");
+
+
+    // X-axis label
+svg.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "end")
+    .attr("x", (w / 2) + padding)
+    .attr("y", h - 6)
+    .text("Tree Age (year)");
+
+// Y-axis label
+svg.append("text")
+    .attr("class", "y label")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 15)
+    .attr("x", -100)
+    .text("Tree Height (m)");
